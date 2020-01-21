663098119aae5b16d19cdcc87f7b8837a68eac46
TomatoSoup <jonathantang0126@gmail.com>
2020-01-21 14:53:11 +0800

    [加入] fs下載圖檔

--- getImgPath.js
+++ getImgPath.js
@@ -1,6 +1,7 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
 
-async function getImgPath(productPath) {
+async function getImgPath(folderName,productPath) {
   try {
 
     const browser = await puppeteer.launch();
@@ -17,6 +18,16 @@ async function getImgPath(productPath) {
       })
       return imgPathArray
     })
+
+    for (let index = 0; index < imgPaths.length; index++) {
+      const imgPath = imgPaths[index]
+      var imgSource = await page.goto(imgPath)
+      //新增資料夾
+      fs.mkdir(`${folderName}`, function (err) {})
+      //下載圖檔
+      fs.writeFile(`./${folderName}/${index}.jpg`, await imgSource.buffer(), function (err){})
+      console.log(`image_${index} download complete`)
+    }
 
     await browser.close()
 
